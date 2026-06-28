3d15f350243e186e650969e98373ab8ff9b853d9
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-28 18:53:57 -0300

    Validando Dados do Usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,21 @@
 // responsavel por lidar com a logica da rota de users
 import { Request, Response } from 'express';
+//importando zod para lidar com os dados
+import { z } from 'zod';
 
 //Agrupando metodos da rota
 class UsersController {
   create(request: Request, response: Response) {
+    //Tratando os dados com o zod
+    const bodySchema = z.object({
+      name: z.string().trim().min(2), // removendo os espacos e aceitando pelo menos 1 caracter
+      email: z.string().email(), // apenas aceitando email
+      password: z.string().min(6), // aceitando no minimo 6 caracteres para senha
+    });
+
+    //Pegando os dados validados do bodyschema para validar os erros
+    const { name, email, password } = bodySchema.parse(request.body);
+
     return response.json({ message: 'ok' });
   }
 }
