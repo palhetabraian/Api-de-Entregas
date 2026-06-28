b7cad63d1c4de5ea47a4414907825e8fed3ec870
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-28 19:01:13 -0300

    Criptografando Senha do Usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,11 +1,15 @@
 // responsavel por lidar com a logica da rota de users
 import { Request, Response } from 'express';
+
+//importando o hash para criptografia
+import { hash } from 'bcrypt';
+
 //importando zod para lidar com os dados
 import { z } from 'zod';
 
 //Agrupando metodos da rota
 class UsersController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     //Tratando os dados com o zod
     const bodySchema = z.object({
       name: z.string().trim().min(2), // removendo os espacos e aceitando pelo menos 1 caracter
@@ -16,7 +20,10 @@ class UsersController {
     //Pegando os dados validados do bodyschema para validar os erros
     const { name, email, password } = bodySchema.parse(request.body);
 
-    return response.json({ message: 'ok' });
+    //gerando criptografia para senha
+    const hashedPassword = await hash(password, 8);
+
+    return response.json({ message: 'ok', hashedPassword });
   }
 }
 
