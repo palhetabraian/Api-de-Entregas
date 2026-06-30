864f8e51383f6fa09ba1b88ff12c01dafe086239
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-06-30 10:44:27 -0300

    Recuperando o Usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,10 +1,40 @@
+import { AppError } from '@/utils/AppError';
 import { Request, Response } from 'express';
+import { prisma } from '@/database/prisma';
+import { compare } from 'bcrypt';
+import { z } from 'zod';
 
 class SessionsController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
+    //validando os dados do usuario
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    // verificando email e senha
+    const { email, password } = bodySchema.parse(request.body);
+
+    // pegando usuario do banco de dados
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    //validando se o usuario existe
+    if (!user) {
+      throw new AppError('Invalid email or password', 401);
+    }
+
+    // Verificando a senha
+    const passwordMatched = await compare(password, user.password); // verifica a senha com a senha passada do usuario dentro do banco
+
+    //verificando se a senha nao bate
+    if (!passwordMatched) {
+      throw new AppError('Invalid email or password', 401);
+    }
+
     return response.json({ message: 'ok' });
   }
 }
 
 export { SessionsController };
- 
