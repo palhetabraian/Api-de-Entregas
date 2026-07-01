9f47d37f2f44f022330b8caa9361c29d4da86aac
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-07-01 14:14:53 -0300

    Controller responsavel pelo cadastro de log do pedido

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,13 +1,45 @@
 // responsavel por logs da entrega
 import { Request, Response } from 'express';
+import z from 'zod';
+import { prisma } from '@/database/prisma';
+import { AppError } from '@/utils/AppError';
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
+    //validando dados
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
 
-    
+    //valida se os dados estao vindo como esperados
+    const { delivery_id, description } = bodySchema.parse(request.body);
 
-    return response.json({ message: 'ok' });
+    //procurando pedido no banco de dados
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    //verificando o pedido se nao existe
+    if (!delivery) {
+      throw new AppError('delivery not found', 404);
+    }
+
+    //verificando se o status ainda ta processando
+    if (delivery.status === 'processing') {
+      throw new AppError('change status to shipped', 404);
+    }
+
+    //adicionando o log do pedido
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
-export {DeliveryLogsController}
+export { DeliveryLogsController };
