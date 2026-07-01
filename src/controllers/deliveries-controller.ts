f0e6cb7872bb409c8d280a407346fa548479605b
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-07-01 12:46:46 -0300

    cadastro de entrega

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from 'express';
+import { prisma } from '@/database/prisma';
+import { z } from 'zod';
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: 'ok' });
+  async create(request: Request, response: Response) {
+    //validando conteudo da rota
+    const bodySchema = z.object({
+      user_id: z.string().uuid(), // id do usuario que vai ser enviado o produto
+      description: z.string(),
+    });
+
+    //recuperando dados
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
